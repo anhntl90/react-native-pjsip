7d199a045cb841ea3b06c3237458bf82694c349c
anhntl <anhntl@s-wifi.vn>
2020-02-11 16:54:53 +0700

    auto start app internet connected

--- android/src/main/java/com/carusto/ReactNativePjSip/PjSipConnectivityReceiver.java
+++ android/src/main/java/com/carusto/ReactNativePjSip/PjSipConnectivityReceiver.java
@@ -6,28 +6,35 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import com.carusto.ReactNativePjSip.utils.ArgumentUtils;
 import com.carusto.ReactNativePjSip.PjSipBroadcastReceiver;
-import com.facebook.react.bridge.ReactApplicationContext;
 import android.net.NetworkInfo;
 import android.util.Log;
 
 public class PjSipConnectivityReceiver extends BroadcastReceiver {
-        private ReactApplicationContext context;
 
         public PjSipConnectivityReceiver() {
 
         }
 
         @Override
-        public void onReceive(Context context, Intent intent) {
+        public void onReceive(Context context, Intent _intent) {
                 ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
                 NetworkInfo netInfo = cm.getActiveNetworkInfo();
-                String json = intent.getStringExtra("data");
+                String json = _intent.getStringExtra("data");
                 if (netInfo != null && netInfo.isConnected()) {
+                        String ns = context.getApplicationContext().getPackageName();
                         json = "{\"connectivity\":true}";
-                        Log.d("ReactNative", "Internet is up!" + json);
+                        Log.d("ReactNative", "Internet is up!");
+                        String cls = ns + ".MainActivity";
+
+                        Intent intent = new Intent();
+                        intent.setClassName(ns, cls);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.EXTRA_DOCK_STATE_CAR);
+                        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                        intent.putExtra("foreground", true);
+                        context.startActivity(intent);
                 } else {
                         json = "{\"connectivity\":false}";
-                        Log.d("ReactNative", "Internet is down!" + json);
+                        Log.d("ReactNative", "Internet is down!");
                 }
                 Object params = ArgumentUtils.fromJson(json);
                 PjSipBroadcastReceiver.emit("pjSipConnectStateChanged", params);
